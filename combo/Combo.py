b37a28b6c264bba75fb55b602b142e2e1977eec5
David <daminton@bsc.edu>
2021-04-07 20:24:46 -0500

    final commit

--- combo/Combo.py
+++ combo/Combo.py
@@ -6,8 +6,8 @@ class App(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master = master
-        master.title('Fibonacci')
-        master.geometry('400x400')
+        master.title('Combo')
+        master.geometry('400x500')
 
         #fibonacci
         self.num_1_label = Label(master, text='Enter a number for y: ')
@@ -31,22 +31,11 @@ class App(Frame):
         self.answer = Label(master, text='')
         self.answer.pack()
 
-        self.submit_button = Button(master, text='Submit', command=self.Fibonacci_with_input) 
+        self.submit_button = Button(master, text='Get Fibonacci', command=self.Fibonacci_with_input) 
         self.submit_button.pack()
 
         #fizzbuzz
-        self.replacement_fizz_label = Label(master, text='Enter a replacement for fizz: ')
-        self.replacement_fizz_label.pack()
 
-        self.replacement_fizz_entry = Entry(master)
-        self.replacement_fizz_entry.pack()
-
-        self.replacement_buzz_label = Label(master, text='Enter a replacement for buzz: ')
-        self.replacement_buzz_label.pack()
-
-        self.replacement_buzz_entry = Entry(master)
-        self.replacement_buzz_entry.pack()
-        
         self.divisor_1_label = Label(master, text='Enter a divisor for fizz: ')
         self.divisor_1_label.pack()
 
@@ -59,10 +48,28 @@ class App(Frame):
         self.divisor_2_entry = Entry(master)
         self.divisor_2_entry.pack()
 
+        self.replacement_fizzbuzz_label = Label(master, text='Enter a replacement for fizzbuzz: ')
+        self.replacement_fizzbuzz_label.pack()
+
+        self.replacement_fizzbuzz_entry = Entry(master)
+        self.replacement_fizzbuzz_entry.pack()
+
+        self.replacement_fizz_label = Label(master, text='Enter a replacement for fizz: ')
+        self.replacement_fizz_label.pack()
+
+        self.replacement_fizz_entry = Entry(master)
+        self.replacement_fizz_entry.pack()
+
+        self.replacement_buzz_label = Label(master, text='Enter a replacement for buzz: ')
+        self.replacement_buzz_label.pack()
+
+        self.replacement_buzz_entry = Entry(master)
+        self.replacement_buzz_entry.pack()
+
         self.answer_2 = Label(master, text='')
         self.answer_2.pack()
 
-        self.submit_button_2 = Button(master, text='Submit', command=self.fizzbuzz_with_input_from_fibonacci) 
+        self.submit_button_2 = Button(master, text='Get Fizzbuzz', command=self.fizzbuzz_with_input_from_fibonacci) 
         self.submit_button_2.pack()
 
 
@@ -70,9 +77,13 @@ class App(Frame):
         try:
             # first two terms
             try:
-                self.n1, self.n2 = int(self.num_1_entry.get()), int(self.num_2_entry.get())
+                self.n1 = int(self.num_1_entry.get())
             except ValueError:
-                self.n1, self.n2 = 1, 1
+                self.n1 = 1
+            try:
+                self.n2 = int(self.num_2_entry.get())
+            except ValueError:
+                self.n2 = 1
             self.count = 0
             global num_list
             num_list = []
@@ -91,30 +102,53 @@ class App(Frame):
                     self.count += 1
             self.answer.config(text=num_list)
         except ValueError:
-            self.answer.config(text='Incorrect input')
+            self.answer.config(text='Enter Integer')
 
     def fizzbuzz_with_input_from_fibonacci(self):
         try:
-            fizzbuzz_list = []
             try:
-                self.divisor_1, self.divisor_2 = int(self.divisor_1_entry.get()), int(self.divisor_2_entry.get())
+                fizzbuzz = self.replacement_fizzbuzz_entry.get()
+                if not fizzbuzz:
+                    raise ValueError('empty String')
             except ValueError:
-                self.divisor_1, self.divisor_2 = 3, 5
+                fizzbuzz = 'fizzbuzz'
+            try:
+                fizz = self.replacement_fizz_entry.get()
+                if not fizz:
+                    raise ValueError('empty String')
+            except ValueError:
+                fizz = 'fizz'
+            try:
+                buzz = self.replacement_buzz_entry.get()
+                if not buzz:
+                    raise ValueError('empty String')
+            except ValueError:
+                buzz = 'buzz'
+            try:
+                fizz_num = int(self.divisor_1_entry.get())
+            except ValueError:
+                fizz_num = 3
+            try:
+                buzz_num = int(self.divisor_2_entry.get())
+            except ValueError:
+                buzz_num = 5
+
+            fizzbuzz_list = []
             global num_list
-            for fizzbuzz in num_list:
-                if fizzbuzz % self.divisor_1== 0 and fizzbuzz % self.divisor_2 == 0:
-                    fizzbuzz_list.append('fizzbuzz')
+            for i in num_list:
+                if i % fizz_num == 0 and i % buzz_num == 0:
+                    fizzbuzz_list.append(fizzbuzz)
                     continue
-                elif fizzbuzz % self.divisor_1 == 0:
-                    fizzbuzz_list.append('fizz')
+                elif i % fizz_num == 0:
+                    fizzbuzz_list.append(fizz)
                     continue
-                elif fizzbuzz % self.divisor_2 == 0:
-                    fizzbuzz_list.append('buzz')
+                elif i % buzz_num == 0:
+                    fizzbuzz_list.append(buzz)
                     continue
-                fizzbuzz_list.append(fizzbuzz)
+                fizzbuzz_list.append(i)
             self.answer_2.config(text=fizzbuzz_list)
-        except ValueError:
-            self.answer_2.config(text='That is not what we are looking for...')
+        except NameError:
+            self.answer_2.config(text='Enter Fibonacci ')
 
     
 a = App(root)
